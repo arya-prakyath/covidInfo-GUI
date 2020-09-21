a3db6edf9a0207fe75775ce9c8ddb7941f6c7d2d
arya <arya.prakyath@gmail.com>
2020-09-21 23:54:41 +0530

    Added state-wise notification. Added beautification. Added close button

--- covidInfo.py
+++ covidInfo.py
@@ -1,4 +1,5 @@
 import requests
+from plyer import notification
 from bs4 import BeautifulSoup
 from tkinter import *
 from tkinter import ttk
@@ -7,6 +8,17 @@ def return_html(url):
     html = requests.get(url)
     html = html.text
     return html
+
+
+def notify(state, table_row):
+    for row in table_row:
+        data = row.find_all('td')
+        if data[0].get_text() == state:
+            title = f"Covid Cases in {state}"
+            message = f"Confirmed Cases: {data[1].get_text()}\nRecovered: {data[2].get_text()}\nDeaths: {data[3].get_text()}"
+            break
+    notification.notify(title=title, message=message, app_icon="icon.ico", timeout=20)
+
 
 def covid_table(table_row):
     # Create a window
@@ -47,8 +59,53 @@ def covid_table(table_row):
         table.insert("", index=END, values=(index, state, confirmed, recovery, death))
         index += 1
 
-def state_wise():
-    pass
+
+def state_wise(table_row):
+    homeFrame.pack_forget()
+    global state_wise_frame, warn_message
+    state_wise_frame = Frame(root)
+    state_wise_frame.pack()
+
+    state_var = StringVar(state_wise_frame)
+    state_list = ['Select State', 'Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam',
+                  'Bihar', 'Chandigarh', 'Chhattisgarh', 'Dadar Nagar Haveli', 'Delhi', 'Goa', 'Gujarat', 'Haryana',
+                  'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerala', 'Ladakh',
+                  'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha',
+                  'Puducherry', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telengana', 'Tripura', 'Uttarakhand',
+                  'Uttar Pradesh', 'West Bengal']
+    state_var.set(state_list[0])
+    state_options = OptionMenu(state_wise_frame, state_var, *state_list)
+    state_options["relief"] = SOLID
+    state_options["cursor"] = "hand2"
+    state_options.configure(font=('lucida', 15))
+    state_options.pack(fill=X, pady=(25, 0))
+    state_options["bg"] = btnbg
+    state_options["activebackground"] = activeBtn
+    state_options["highlightbackground"] = activeBtn
+
+    def validate():
+        if state_var.get() == "Select State":
+            warn_message.config(text="Please select a State")
+        else:
+            warn_message.config(text="")
+            notify(state_var.get(), table_row)
+
+    info_button = Button(state_wise_frame, text="Get Info", width="10",  anchor=CENTER, cursor="hand2", relief=SOLID, bg=btnbg, fg=btnfg,  activebackground=activeBtn, font=('lucida', 15), command=validate)
+    info_button.pack(side=LEFT, pady=25, padx=15)
+
+    back_button = Button(state_wise_frame, text="Back", width="10", anchor=CENTER, cursor="hand2", relief=SOLID, bg=btnbg, fg=btnfg,  activebackground=activeBtn, font=('lucida', 15), command=back)
+    back_button.pack(side=RIGHT, pady=25, padx=15)
+
+    warn_message = Label(root, font=('lucida', 12, 'bold'), fg="red")
+    warn_message.pack(fill=Y)
+
+
+def back():
+    global state_wise_frame, warn_message
+    state_wise_frame.destroy()
+    warn_message.destroy()
+    homeFrame.pack(fill=BOTH, expand=True)
+
 
 if __name__ == '__main__':
     # Fetch data
@@ -56,22 +113,38 @@ if __name__ == '__main__':
     covidDataHTML = BeautifulSoup(covidHTML, 'html.parser')
     # Access table
     tableData = covidDataHTML.find_all('tbody')[0]
-    # Access rows - Satae wise
+    # Access rows - State wise
     tableRow = tableData.find_all('tr')[0:35+1]
 
 
     root = Tk()
     root.title("CovidInfo | THE_ARYA")
-    root.geometry("500x200")
+    root.wm_iconbitmap("icon.ico")
+    root.geometry("500x300")
     root.resizable(False, False)
+    bg = "#FBEAEB"
+    fg = "#1D1B1B"
+    btnbg = "#a6a6a6"
+    btnfg = "#1D1B1B"
+    activeBtn = "#4d4d4d"
+    root['bg'] = "#FBEAEB"
 
-    title = Label(root, text="Covid Info - India(State Wise)", font=('lucida', 20))
-    title.pack(side='top', fill=X, pady=(15, 0))
+    title = Label(root, text="Covid Info - India(State Wise)", font=('lucida', 20), bg=bg, fg=fg)
+    title.pack(side='top', fill=X, pady=(25, 0))
 
-    tableBtn = Button(root, text="Table", command=lambda :covid_table(tableRow), width="50", anchor=CENTER, relief=SOLID)
-    tableBtn.pack(pady=15)
+    homeFrame = Frame(root)
+    homeFrame.pack(fill=BOTH, expand=True)
 
-    stateBtn = Button(root, text="Choose State", command=state_wise, width="50", anchor=CENTER, relief=SOLID)
-    stateBtn.pack(pady=10)
+    tableBtn = Button(homeFrame, text="View Table", width="50", font=('lucida', 12), anchor=CENTER, cursor="hand2", relief=SOLID, bg=btnbg, fg=btnfg, activebackground=activeBtn, command=lambda: covid_table(tableRow))
+    tableBtn.pack(pady=(25, 0))
+
+    stateBtn = Button(homeFrame, text="Choose State", font=('lucida', 12), width="50", anchor=CENTER, cursor="hand2", relief=SOLID, bg=btnbg, fg=btnfg,  activebackground=activeBtn, command=lambda: state_wise(tableRow))
+    stateBtn.pack(pady=(15, 0))
+
+    closeBtn = Button(homeFrame, text="Close", font=('lucida', 12), width="50", anchor=CENTER, cursor="hand2", relief=SOLID, bg=btnbg, fg=btnfg,  activebackground=activeBtn, command=root.destroy)
+    closeBtn.pack(pady=(15, 0))
+
+    footer = Label(root, text="Copyrights - THE_ARYA 2020", anchor=CENTER, fg="BLACK", bg="#99b3e6")
+    footer.pack(side=BOTTOM, fill=X, pady=0, ipady=5)
 
     root.mainloop()
